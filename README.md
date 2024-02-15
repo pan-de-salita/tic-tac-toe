5e213c1ad8c781576e0e2a735da2004bd97f42bb
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-02-15 22:09:01 +0800

    added function to display current game state

---
+++ README.md
@@ -0,0 +1,1 @@
+an interactive tic-tac-toe game
---
+++ script.js
@@ -0,0 +1,29 @@
+const gameDisplay = document.querySelector('.game-display');
+
+let currentGameState = [
+  ['X', 'O', 'X'],
+  ['X', 'O', ''],
+  ['O', 'O', ''],
+];
+
+// displays current game state
+const displayGameState = (gameState) => {
+  gameState.map(row => row.map(cell => displayCell(cell)));
+}
+
+// displays a single cell
+const displayCell = (cell) => {
+  gameDisplay.append(createCell(cell));
+};
+
+// creates a single cell
+const createCell = (cell) => {
+  return Object.assign(
+    document.createElement('div'), {
+      className: 'cell',
+      style: 'border:  1px solid blue',
+      textContent: cell
+    });
+}
+
+displayGameState(currentGameState);
