109f7998e2fb1829a52901840b530550958d1850
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-02-21 20:57:35 +0800

    made refresh button functional

--- init.js
+++ init.js
@@ -4,12 +4,14 @@ import { handleCell } from './scripts/handleCell.js';
 import { currentGameRecord } from './scripts/updateCurrentGameState.js';
 import { currentGameState } from './scripts/updateCurrentGameState.js';
 
+const RESTART_BTN = document.querySelector('.restart');
+
 renderGameState();
 export const CELLS = document.querySelectorAll('.cell');
 CELLS.forEach(cell => cell.addEventListener('click', handleCell));
 
-window.onload = function() {
-  newGame();
-  console.log(`currentGameRecord: ${currentGameRecord}`);
-  console.log(`currentGameState: ${currentGameState}`);
-};
+RESTART_BTN.addEventListener('click', function() {
+  location.reload();
+});
+
+window.onload = newGame();
